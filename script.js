e71d0536505358a23fc449c94c65dad1549d403c
01mehran <132054519+01mehran@users.noreply.github.com>
2024-07-24 15:05:23 +0330

    Set Check Input

--- script.js
+++ script.js
@@ -4,31 +4,38 @@ const Box = document.getElementById("box");
 
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
-
-  inputs.forEach((value) => {
-    const inputValue = value.value;
-    const newLi = document.createElement("span");
-    newLi.innerHTML = inputValue;
-    Box.appendChild(newLi);
+  let empty = false;
+  inputs.forEach((e) => {
+    if (e.value === "") {
+      empty = true;
+    }
   });
+  if (empty) {
+    alert("Please fill out all inputs");
+  } else {
+    inputs.forEach((value) => {
+      const inputValue = value.value;
+      const newLi = document.createElement("span");
+      newLi.innerHTML = inputValue;
+      Box.appendChild(newLi);
+      console.log(inputValue);
+      value.value = "";
+    });
 
-  const btn1 = document.createElement("button");
-  const btn2 = document.createElement("button");
-  const btn3 = document.createElement("button");
-  btn1.innerText = "حذف";
-  btn2.innerText = "ویرایش";
-  btn3.innerText = "جزییات";
-  Box.appendChild(btn1);
-  Box.appendChild(btn2);
-  Box.appendChild(btn3);
+    const btn1 = document.createElement("button");
+    const btn2 = document.createElement("button");
+    const btn3 = document.createElement("button");
+    btn1.innerText = "حذف";
+    btn2.innerText = "ویرایش";
+    btn3.innerText = "جزییات";
 
-  const title = document.getElementById("title").value;
-  const date = document.getElementById("date").value;
-  const explain = document.getElementById("explain").value;
-
-  localStorage.setItem("title", title);
-  localStorage.setItem("date", date);
-  localStorage.setItem("explaintion", explain);
+    btn1.classList.add("delete");
+    btn2.classList.add("edit");
+    btn3.classList.add("detials");
+    Box.appendChild(btn1);
+    Box.appendChild(btn2);
+    Box.appendChild(btn3);
+  }
 });
 
 // https://stackoverflow.com/questions/72591283/creating-an-edit-buttonto-do-app-javascript
